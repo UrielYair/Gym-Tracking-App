97189f4ab51eab5e789025d7970f05d023423d4d
Uriel <uriel91@gmail.com>
2019-02-25 00:51:59 +0200

    Remove unnecessary members from RoutingComponent class.

--- FinalProject/src/main/java/com/hit/controller/RoutingComponent.java
+++ FinalProject/src/main/java/com/hit/controller/RoutingComponent.java
@@ -14,60 +14,73 @@ import org.apache.log4j.Logger;
 /**
  * Servlet implementation class RoutingComponent
  */
+
 @WebServlet("/controller/*")
 public class RoutingComponent extends HttpServlet {
+	
+	/* *
+	 * Responsible for routing requests to the appropriate controller class based on the MVC design pattern.
+	 * The algorithm will make decisions by the URL path using split on the '/' delimiter.
+	 * */
+	
 	private static final long serialVersionUID = 1L;
 	private static final Logger LOGGER = Logger.getLogger(RoutingComponent.class.getSimpleName());
 	
-	private String path;
-	private String[] parts;
-	private String controllerName;
-	private String actionName;
-	private String strAfterAction;
+	// TODO: uncomment if needed:
+	
+	// private String path;
+	// private String[] parts;
+	// private String controllerName;
+	// private String actionName;
+	// private String strAfterAction;
 
 	public RoutingComponent() {
 		super();
 	}
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) {
-		path = request.getPathInfo();
+		
+		String path= request.getPathInfo();
 		LOGGER.info("Message from: " + path);
-		parts = path.split("/");
+		String[] parts = path.split("/");
+		
 		try {
-			if (parts.length > 2) {
-				LOGGER.info("Pass parts length > 2");
-				controllerName = parts[1];
-				actionName = parts[2];
-				LOGGER.info("Controller name: " + controllerName + ", action name: " + actionName);
-				strAfterAction = null;
-				if (parts.length >= 4) {
-					LOGGER.info("Pass parts length >= 4");
-					strAfterAction = path.substring(parts[1].length() + parts[2].length() + 2);
-					LOGGER.info("strAfterAction: " + strAfterAction);
-				} else {
-					LOGGER.info("Pass patrs = 3");
-					Class controllerClass = Class.forName("com.hit.controller." + controllerName + "Controller");
-					Object controller = controllerClass.newInstance();
-					Method method = controllerClass.getMethod(actionName, HttpServletRequest.class, HttpServletResponse.class);
-					method.invoke(controller, request, response);
-				}
-			} else {
-				LOGGER.info("wrong url");
-				response.getWriter().append("ERROR: Wrong URL");
+			
+			String controllerName= parts[1];
+			String actionName = parts[2];
+					
+			if (parts.length == 3) {
+				LOGGER.info("URL contain exactly 3 parts separated by '/'");
+				Class<?> controllerClass = Class.forName("com.hit.controller." + controllerName + "Controller");
+				Object controller = controllerClass.newInstance();
+				Method method = controllerClass.getMethod(actionName, HttpServletRequest.class, HttpServletResponse.class);
+				method.invoke(controller, request, response);
+				return;
 			}
-		} catch (IllegalAccessException illegalAccessException) {
+	
+			else {
+				// Invalid URL: URL can not contain other than 3 parts.
+				LOGGER.info("Invalid URL");
+				response.getWriter().append("ERROR: Invalid URL");
+			}
+		} 
+		catch (IllegalAccessException illegalAccessException) {
 			LOGGER.fatal("Error, illegalAccessException");
 			illegalAccessException.printStackTrace();
-		} catch (InvocationTargetException invocationTargetException) {
+		} 
+		catch (InvocationTargetException invocationTargetException) {
 			LOGGER.fatal("Error, InvocationTargetExceptio");
 			invocationTargetException.printStackTrace();
-		} catch (IllegalArgumentException illegalArgumentException) {
+		} 
+		catch (IllegalArgumentException illegalArgumentException) {
 			LOGGER.fatal("Error, IllegalArgumentException");
 			illegalArgumentException.printStackTrace();
-		} catch (IOException ioException) {
+		} 
+		catch (IOException ioException) {
 			LOGGER.fatal("Error, IOException");
 			ioException.printStackTrace();
-		} catch (Exception exception) {
+		} 
+		catch (Exception exception) {
 			LOGGER.fatal("Error, unknown Exception");
 			exception.printStackTrace();
 		}
