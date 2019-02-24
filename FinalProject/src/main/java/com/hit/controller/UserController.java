793bc753bd29d2395f787032df873324cef57d0e
Uriel <uriel91@gmail.com>
2019-02-25 01:43:22 +0200

    Remove unnecessary members from userController + add documentation.

--- FinalProject/src/main/java/com/hit/controller/UserController.java
+++ FinalProject/src/main/java/com/hit/controller/UserController.java
@@ -15,88 +15,109 @@ import com.hit.utils.InputValidator;
 
 public class UserController{
 	private static final Logger LOGGER = Logger.getLogger(UserController.class.getSimpleName());
+	/**
+	 * The class will be responsible for receiving information regarding users in the system.
+	 * The class will forward the information to the Model classes which handle them by needs.
+	 **/
 
-	private String userName;
-	private String password;
-	private PrintWriter printWriter;
-	private HibernateGymDAO hibernateGymDAO;
-	private User user;
 	private InputValidator inputValidator;
 
 	public UserController() {
-		LOGGER.debug("Creating userController instance");
-		this.userName = null;
-		this.password = null;
-		this.printWriter = null;
 		this.inputValidator = new InputValidator();
-		this.hibernateGymDAO = HibernateGymDAO.getInstance();
-		this.user = null;
 	}
 
 	public void register(HttpServletRequest request, HttpServletResponse response) {
-		LOGGER.info("Register request");
-		userName = request.getParameter("username");
-		password = request.getParameter("password");
-		LOGGER.info("Going to reginster new user: username = " + userName + "password = " + password);
-		if (inputValidator.usernameValidation(userName) && inputValidator.passwordValidation(password)) {
-			LOGGER.info("Input is valid");
-			try {
-				printWriter = response.getWriter();
-				user = hibernateGymDAO.getUser(userName);
-				if (user == null) {
-					LOGGER.info("User does not exist at DB. Going to create new one...");
-					if (hibernateGymDAO.addUser(new User(userName, password))) {
-						LOGGER.info("User added succesfully");
-						printWriter.println("Registration completed successfully");
-					} else {
-						LOGGER.info("User was not added");
-						printWriter.println("Registration failed");
-					}
-				} else {
-					LOGGER.info("Username already taken");
-					printWriter.println("Username already taken, Please choose another one.");
-				}
-			} catch (IOException ioException) {
-				LOGGER.fatal("Error, IOException!");
-				ioException.printStackTrace();
-			} catch (Exception exception) {
-				LOGGER.fatal("Error, unknown Exception!");
-				exception.printStackTrace();
-			} finally {
-				LOGGER.fatal("Going to close writer");
-				printWriter.close();
-			}
-		} else {
+		/**
+		 * Method for registering users in the system.
+		 * Existence check will be taken prior registration.
+		 * */
+		
+		String userName = request.getParameter("username");
+		String password = request.getParameter("password");
+		LOGGER.info("Register request for new user: username = " + userName + "password = " + password);
+		
+		if (!inputValidator.usernameValidation(userName) || !inputValidator.passwordValidation(password)) {
 			LOGGER.info("Input is not valid");
+			return;
 		}
-	}
-
-	public void login(HttpServletRequest request, HttpServletResponse response) {
-		userName = request.getParameter("username");
-		password = request.getParameter("password");
-		LOGGER.info("Login user - userName = " + userName + " password = " + password);
+		
+		LOGGER.info("Input is valid");
+		PrintWriter printWriter = null;
+		
 		try {
-			if (inputValidator.usernameValidation(userName) && inputValidator.passwordValidation(password)) {
-				LOGGER.info("Input is valid");
-				printWriter = response.getWriter();
-				user = hibernateGymDAO.getUser(userName);
-				if (user != null) {
-					LOGGER.info("User exist, going to check password");
-					if (user.getPassword().equals(password)) {
-						LOGGER.info("password correct, going to login...");
-						printWriter.println("Login successfully");
-						LOGGER.info("Login successfully");
-					} else {
-						LOGGER.info("password is not correct!");
-						//printWriter.println("Wrong password, please retry");
-					}
-				} else {
-					LOGGER.debug("User does not exist at DB");
-					printWriter.println("User dosen't exist, please register before login");
+			
+			printWriter = response.getWriter();
+			User user = HibernateGymDAO.getInstance().getUser(userName);
+			if (user == null) {
+				
+				LOGGER.info("User does not exist at DB. Going to create new one...");
+				
+				if (HibernateGymDAO.getInstance().addUser(new User(userName, password))) {
+					LOGGER.info("User added succesfully");
+					printWriter.println("Registration completed successfully");
+				} 
+				else {
+					LOGGER.info("User was not added");
+					printWriter.println("Registration failed");
 				}
-			} else {
-				LOGGER.info("Input is not valid");
+			} 
+			else {
+				LOGGER.info("Username already taken");
+				printWriter.println("Username already taken, Please choose another one.");
 			}
+		} 
+		
+		catch (IOException ioException) {
+			LOGGER.fatal("Error, IOException!");
+			ioException.printStackTrace();
+		} 
+		catch (Exception exception) {
+			LOGGER.fatal("Error, unknown Exception!");
+			exception.printStackTrace();
+		} 
+		
+		finally {
+			printWriter.close();
+		}
+	} 
+	
+	public void login(HttpServletRequest request, HttpServletResponse response) {
+		/**
+		 * Method is used to verify user while trying to login
+		 * */
+		
+		String userName = request.getParameter("username");
+		String password = request.getParameter("password");
+		LOGGER.info("Login user - userName = " + userName + " password = " + password);
+		PrintWriter printWriter = null;
+		
+		try {
+
+			if (!inputValidator.usernameValidation(userName) || !inputValidator.passwordValidation(password)) {
+				LOGGER.info("Input is not valid");
+				return;
+			}
+				
+			printWriter = response.getWriter();
+			User user = HibernateGymDAO.getInstance().getUser(userName);
+			
+			if (user != null) {
+				
+				if (user.getPassword().equals(password)) {
+					LOGGER.info(userName + " Logged on successfully.");
+					printWriter.println("Login successfully");
+				} 
+				else {
+					LOGGER.info(userName + " entered wrong password.");
+					printWriter.println("Wrong password, please retry");
+				}
+			} 
+			
+			else {
+				LOGGER.debug("Loggin attempt by not existing user: " + userName);
+				printWriter.println("User dosen't exist, please register before login");
+			}
+			 
 		} catch (IOException ioException) {
 			LOGGER.fatal("Error, IOException!");
 			ioException.printStackTrace();
@@ -104,54 +125,71 @@ public class UserController{
 			LOGGER.fatal("Error, unknown Exception!");
 			exception.printStackTrace();
 		} finally {
-			LOGGER.fatal("Going to close writer");
 			printWriter.close();
 		}
 	}
 
 	public void logout(HttpServletRequest request, HttpServletResponse response) {
 		HttpSession session = request.getSession();
+		PrintWriter printWriter = null;
+		
 		try {
 			printWriter = response.getWriter();
-			if (session.getAttribute("userName") != null) { // check if user connected to the system
+			
+			if (session.getAttribute("userName") != null) { // TODO: implement checking if user connected to the system here.
 				session.invalidate();
-				LOGGER.info("Loguot successfully");
+				LOGGER.info("Logout successfully"); // TODO: fix string.
 				printWriter.println("Logout successfully");
-			} else {
+			} 
+			else {
 				LOGGER.info("you are not connected!");
 				printWriter.println("you are not connected!");
 			}
-		} catch (IOException ioException) {
+		} 
+		
+		catch (IOException ioException) {
 			LOGGER.fatal("Error, IOException!");
 			ioException.printStackTrace();
-		} catch (Exception exception) {
+		} 
+		catch (Exception exception) {
 			LOGGER.fatal("Error, unknown Exception!");
 			exception.printStackTrace();
-		} finally {
-			LOGGER.fatal("Going to close writer");
+		}
+		
+		finally {
 			printWriter.close();
 		}
 	}
 
 	public void deleteUser(HttpServletRequest request, HttpServletResponse response) {
+		
 		HttpSession session = request.getSession();
+		PrintWriter printWriter =null;
+		
 		try {
-			PrintWriter writer = response.getWriter();
+			printWriter = response.getWriter();
 			if (session.getAttribute("username") != null) {
-				hibernateGymDAO.deleteUser(userName);
+				HibernateGymDAO.getInstance().deleteUser(userName); // TODO: check where should userName should come from. 
 				session.invalidate();
-				writer.println("The user was deleted successfully");
+				printWriter.println("The user was deleted successfully");
 
-			} else {
-				writer.println("you are not connected!");
+			} 
+			else {
+				printWriter.println("you are not connected!");
 			}
-		} catch (IOException ioException) {
+			
+		} 
+		
+		catch (IOException ioException) {
 			LOGGER.fatal("Error, IOException!");
 			ioException.printStackTrace();
-		} catch (Exception exception) {
+		} 
+		catch (Exception exception) {
 			LOGGER.fatal("Error, unknown Exception!");
 			exception.printStackTrace();
-		} finally {
+		} 
+		
+		finally {
 			LOGGER.fatal("Going to close writer");
 			printWriter.close();
 		}
